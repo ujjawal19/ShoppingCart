a10d845109e77a8d01b8e469b97a1b1dba02f602
Ujjawal Mathur <ujjawalmathur.mathur@gmail.com>
2022-04-11 10:46:19 +0530

    AddRemoveUpdateCart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React,{useState} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "react-toastify/dist/ReactToastify.css"
-import logo from './logo.svg';
+
 import './App.css';
-import { toast } from 'react-toastify';
+import {Container,Row,Col} from "reactstrap"
+import {ToastContainer, toast} from "react-toastify"
 import BuyPage from './Components/BuyPage';
+import Cart from './Components/Cart';
 
 function App() {
 
@@ -17,9 +19,10 @@ function App() {
       return array.id === item.id // whther the element which we try to add(array) is already present in cart or not(item)
     }))
     if(isAlreadyAdded !== -1){
-      toast('already added in car',{
+      toast('already added in cart',{
         type: 'error'
-      })
+      });
+      return;
     }
     setCartItem([...cartItem,item]);
   }
@@ -41,9 +44,21 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <BuyPage addInCart={{addInCart}}></BuyPage>
-    </div>
+    // <div className="App">
+    //   <BuyPage addInCart={{addInCart}}></BuyPage>
+    // </div>
+
+    <Container fluid>
+      <ToastContainer/>
+      <Row>
+        <Col md="8">
+          <BuyPage addInCart={addInCart}/>
+        </Col>
+        <Col md="4">
+          <Cart cartItem={cartItem} removeItem={removeItem} buyNow={buyNow}/>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
